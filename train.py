7a9aeaabd84fc254c5cdba038d794395f1b68084
Yuki Zhang <110796546+yuki172-zhang@users.noreply.github.com>
2025-09-13 19:56:58 -0700

    mlflow

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from torcheval.metrics import (
     MulticlassRecall,
     MulticlassF1Score,
 )
+import mlflow
 
 parser = argparse.ArgumentParser()
 
@@ -41,6 +42,12 @@ parser.add_argument(
 )
 
 # data
+parser.add_argument(
+    "--augment",
+    type=int,
+    dest="augment",
+    help="augment dataset",
+)
 parser.add_argument(
     "--max_samples",
     type=int,
@@ -70,6 +77,14 @@ parser.add_argument(
     help="pretrained path for model and optimizer",
 )
 
+parser.add_argument(
+    "--on_kaggle",
+    type=bool,
+    dest="on_kaggle",
+    help="running on kaggle",
+)
+
+
 # eval
 parser.add_argument(
     "--eval_epochs",
@@ -91,6 +106,7 @@ def train(
     eval_epochs=1,
     save_base_folder="trained/",
     max_samples=None,
+    augment=True,
     pretrained_path=None,
 ):
     for name, arg in [
@@ -106,7 +122,9 @@ def train(
         print(name, arg)
 
     model = DetectionModel()
-    train_set = PneumoniaDetectionDataset(split="train", augment=True, normalize=True)
+    train_set = PneumoniaDetectionDataset(
+        split="train", augment=augment, normalize=True
+    )
     eval_set = PneumoniaDetectionDataset(split="val", augment=False, normalize=True)
     loss_fn = nn.CrossEntropyLoss()
 
@@ -149,6 +167,7 @@ def train(
 
     metrics = {
         "train_loss": [],
+        "train_accuracy": [],
         "eval_loss": [],
         "eval_accuracy": [],
         "eval_precision": [],
@@ -158,7 +177,10 @@ def train(
     }
 
     metrics_path = f"{save_base_folder}metrics.json"
-    best_so_far_path = f"{save_base_folder}best_so_far"
+
+    def log_metric(metric, value, epoch):
+        metrics[metric].append([epoch, value])
+        mlflow.log_metric(metric, value, step=epoch)
 
     try:
         with open(metrics_path, "r") as file:
@@ -171,94 +193,108 @@ def train(
         print(f"\nEpoch: {epoch}")
 
         train_loss = 0
+        train_accuracy_metric = MulticlassAccuracy(num_classes=2, average="micro")
 
         progress_bar = tqdm(range(train_num_batches), position=0, leave=False)
 
         model.train()
 
-        for step, (data, labels, _) in enumerate(train_loader):
+        with mlflow.start_run():
+            mlflow.log_param("epochs", epochs)
+            mlflow.log_param("batch_size", batch_size)
+            mlflow.log_param("lr", learning_rate)
+            mlflow.log_param("model", "resnet50")
+            mlflow.log_param("augment", augment)
+            for step, (data, labels, _) in enumerate(train_loader):
 
-            data = data.to(device)
-            labels = labels.to(device)
+                data = data.to(device)
+                labels = labels.to(device)
 
-            optimizer.zero_grad()
+                optimizer.zero_grad()
 
-            predictions = model.forward(data)
-            loss = loss_fn(predictions, labels)
+                logits = model.forward(data)
+                loss = loss_fn(logits, labels)
 
-            loss.backward()
-            optimizer.step()
+                loss.backward()
+                optimizer.step()
 
-            progress_bar.update(1)
+                progress_bar.update(1)
 
-            train_loss += loss.item() / train_num_batches
+                train_loss += loss.item() / train_num_batches
+                pred_labels = torch.argmax(logits, dim=1)
+                train_accuracy_metric.update(pred_labels, labels)
 
-            del data, labels
+                del data, labels
 
-        metrics["train_loss"].append([epoch, train_loss])
+            log_metric("train_loss", train_loss, epoch)
+            log_metric("train_accuracy", train_accuracy_metric.compute().item(), epoch)
 
-        if (epoch + 1) % save_epochs == 0:
+            if (epoch + 1) % save_epochs == 0:
 
-            eval_loss = 0
-            accuracy_metric = MulticlassAccuracy(num_classes=2, average="micro")
-            precision_metric = MulticlassPrecision(num_classes=2, average=None)
-            recall_metric = MulticlassRecall(num_classes=2, average=None)
-            f1_metric = MulticlassF1Score(num_classes=2, average=None)
-            model.eval()
-            with torch.no_grad():
-                for step, (data, labels, _) in enumerate(eval_loader):
+                eval_loss = 0
+                accuracy_metric = MulticlassAccuracy(num_classes=2, average="micro")
+                precision_metric = MulticlassPrecision(num_classes=2, average=None)
+                recall_metric = MulticlassRecall(num_classes=2, average=None)
+                f1_metric = MulticlassF1Score(num_classes=2, average=None)
+                model.eval()
+                with torch.no_grad():
+                    for step, (data, labels, _) in enumerate(eval_loader):
 
-                    data = data.to(device)
-                    labels = labels.to(device)
+                        data = data.to(device)
+                        labels = labels.to(device)
 
-                    logits = model.forward(data)
-                    loss = loss_fn(logits, labels)
+                        logits = model.forward(data)
+                        loss = loss_fn(logits, labels)
 
-                    eval_loss += loss.item() / eval_num_batches
+                        eval_loss += loss.item() / eval_num_batches
 
-                    pred_labels = torch.argmax(predictions, dim=1)
+                        pred_labels = torch.argmax(logits, dim=1)
 
-                    # print("---")
-                    # print(logits)
-                    # print("pred_labels", list(pred_labels.numpy()))
-                    # print("labels", list(labels.numpy()))
+                        # print("---")
+                        # print(logits)
+                        # print("pred_labels", list(pred_labels.numpy()))
+                        # print("labels", list(labels.numpy()))
 
-                    accuracy_metric.update(pred_labels, labels)
-                    precision_metric.update(pred_labels, labels)
-                    recall_metric.update(pred_labels, labels)
-                    f1_metric.update(pred_labels, labels)
+                        accuracy_metric.update(pred_labels, labels)
+                        precision_metric.update(pred_labels, labels)
+                        recall_metric.update(pred_labels, labels)
+                        f1_metric.update(pred_labels, labels)
 
-                    del data, labels
+                        del data, labels
 
-            metrics["eval_loss"].append([epoch, eval_loss])
-            metrics["eval_accuracy"].append([epoch, accuracy_metric.compute().item()])
-            metrics["eval_precision"].append(
-                [epoch, precision_metric.compute()[0].item()]
-            )
-            metrics["eval_recall"].append([epoch, recall_metric.compute()[0].item()])
-            metrics["eval_f1"].append([epoch, f1_metric.compute()[0].item()])
+                log_metric("eval_loss", eval_loss, epoch)
+                log_metric("eval_accuracy", accuracy_metric.compute().item(), epoch)
+                log_metric(
+                    "eval_precision", precision_metric.compute()[0].item(), epoch
+                )
+                log_metric("eval_recall", recall_metric.compute()[0].item(), epoch)
 
-            with open(f"{save_base_folder}metrics.json", "w") as f:
-                json.dump(metrics, f)
+                os.makedirs(os.path.dirname(save_base_folder), exist_ok=True)
 
-            if (
-                not metrics["best_eval_loss"]
-                or metrics["best_eval_loss"][1] > eval_loss
-            ):
+                with open(f"{save_base_folder}metrics.json", "w") as f:
+                    json.dump(metrics, f)
 
-                metrics["best_eval_loss"] = [epoch, eval_loss]
+                if (
+                    not metrics["best_eval_loss"]
+                    or metrics["best_eval_loss"][1] > eval_loss
+                ):
+                    if metrics["best_eval_loss"]:
+                        prev_epoch = metrics["best_eval_loss"][0]
+                        prev_best_path = f"{save_base_folder}epoch_{prev_epoch}"
+                        if os.path.exists(prev_best_path):
+                            shutil.rmtree(prev_best_path)
 
-                if os.path.exists(best_so_far_path):
-                    shutil.rmtree(best_so_far_path)
+                    metrics["best_eval_loss"] = [epoch, eval_loss]
 
-                os.makedirs(os.path.dirname(best_so_far_path), exist_ok=True)
+                    curr_best_path = f"{save_base_folder}epoch_{epoch}/"
+                    os.makedirs(os.path.dirname(curr_best_path), exist_ok=True)
 
-                torch.save(model, f"{best_so_far_path}model.pth")
-                checkpoint = {
-                    "epoch": epoch,
-                    "optimizer": optimizer.state_dict(),
-                }
-                torch.save(checkpoint, f"{best_so_far_path}checkpoint.pth")
+                    torch.save(model, f"{curr_best_path}/model.pth")
+                    checkpoint = {
+                        "epoch": epoch,
+                        "optimizer": optimizer.state_dict(),
+                    }
+                    torch.save(checkpoint, f"{curr_best_path}/checkpoint.pth")
 
     print("\nEnd training\n")
 
@@ -270,6 +306,7 @@ train(
     save_epochs=args.save_epochs,
     eval_epochs=args.eval_epochs,
     save_base_folder=args.save_base_folder,
+    augment=args.augment,
     max_samples=args.max_samples,
     pretrained_path=args.pretrained_path,
 )
